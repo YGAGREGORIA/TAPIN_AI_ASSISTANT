885a8dcac17eaf2769e346600b03c89423c61f09
Navid Amiri <navidamiri130@gmail.com>
2026-03-03 14:26:38 +0100

    removed email_address from user because it had 2 emails

---
+++ db/migrate/20260303132431_remove_email_adress_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveEmailAdressFromUsers < ActiveRecord::Migration[8.1]
+  def change
+    remove_column :users, :email_address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_03_131435) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_03_132431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -74,7 +74,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_03_131435) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
-    t.string "email_address"
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
